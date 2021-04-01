963870f961eba34b55159f7e53ec7b9cad7b1fd8
fehmi-enes-eren <76635233+fehmi-enes-eren@users.noreply.github.com>
2021-03-31 19:07:13 -0700

    trying some changes

--- ToDo-Backend-main/server.js
+++ ToDo-Backend-main/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://127.0.0.1:5500"
+  origin: "http://127.0.0.1:5500",
 };
 
 app.use(cors(corsOptions));
@@ -21,12 +21,12 @@ const db = require("./app/models");
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Connected to the database!");
   })
-  .catch(err => {
+  .catch((err) => {
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
